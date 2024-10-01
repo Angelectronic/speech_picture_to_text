7b1eefe44dae5e8ab0071cb05a41bc93b19eab82
Phí Minh Hiếu <98246591+Angelectronic@users.noreply.github.com>
2024-10-01 19:00:36 +0700

    chore

--- python/api_ocr.py
+++ python/api_ocr.py
@@ -5,6 +5,8 @@ from pdf2images import *
 from pydantic import BaseModel
 from typing import List
 import os
+import json
+from fastapi.responses import JSONResponse
 
 ocr = OCR(thresh=0.5, visible=False)
 
@@ -27,15 +29,34 @@ async def read_root():
     return {"Hello": "World"}
 
 @app.post("/login")
+@app.post("/login")
 async def login(body: dict):
-    users = [("admin", "123")]
+    users = json.load(open("users.json", "r"))
 
     username = body.get("username")
     password = body.get("password")
-    if (username, password) in users:
-        return {"status": "success"}
-    else: # status 401: Unauthorized
-        raise HTTPException(status_code=401, detail="Incorrect username or password")
+
+    if username not in [user["username"] for user in users]:
+        return JSONResponse(status_code=404, content={"message": "User not found"})
+    user = [user for user in users if user["username"] == username][0]
+    if user["password"] != password:
+        return JSONResponse(status_code=401, content={"message": "Incorrect password"})
+    return {"status": "success"}
+
+@app.post("/register")
+async def register(body: dict):
+    users = json.load(open("users.json", "r"))
+
+    username = body.get("username")
+    password = body.get("password")
+    print(username, password)
+
+    if username in [user["username"] for user in users]:
+        return JSONResponse(status_code=400, content={"message": "User already exists"})
+    users.append({"username": username, "password": password})
+    json.dump(users, open("users.json", "w"))
+    return {"status": "success"}
+
 
 @app.post("/getImg")
 async def get_img(file: UploadFile = File(...)):
